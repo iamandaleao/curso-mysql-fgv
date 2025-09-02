e7cd61a9bd3e1ebed2c7a52b0d69a260abe50999
iamandaleao <amandabibiano144@gmail.com>
2025-09-02 18:14:48 -0300

    feat: add question

---
+++ mod1/uni_3/q6/consulta.sql
@@ -0,0 +1,5 @@
+-- Questão 6 - Indique se há países em comum nos resultados das questões anteriores.
+
+-- Gabarito: Na tabela que consolida todos os resultados, com os países dispostos em ordem alfabética, podemos perceber que Niger e Somália aparecem entre os Top 10 nas cinco consultas realizadas. As conclusões ficam ao seu critério. A tabela foi montada manualmente a partir do resultado das cinco consultas. As setas ao lado do título da coluna indicam se os países estão entre os maiores no quesito ↑ ou entre os menores ↓.
+
+-- Este exercício não se trata exatamente de SQL, mas sim do que pode ser feito a partir de consultas SQL.
