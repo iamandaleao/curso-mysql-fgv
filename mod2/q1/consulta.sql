ddd0706750998137e6a8bd95a7d14418a56f1ed7
iamandaleao <amandabibiano144@gmail.com>
2025-09-03 11:30:15 -0300

    feat: add question

---
+++ mod2/q1/consulta.sql
@@ -0,0 +1,4 @@
+-- Questão 1 - Utilizando a tabela country, o campo four_regions igual a asia e a tabela man_years_at_school, encontre os cinco países asiáticos cujos homens de 25 anos passaram menos tempo na escola em 2009.
+
+
+
