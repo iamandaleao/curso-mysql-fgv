5964c168bb9669fd5698c4412676c911cf8f39a7
iamandaleao <amandabibiano144@gmail.com>
2025-09-16 20:05:44 -0300

    feat: add question

---
+++ mod3/uni_3/q3/consulta.sql
@@ -0,0 +1,2 @@
+-- Questão 3 - Após calcular novamente a média da diferença no tempo de escolaridade entre homens e mulheres no ano 2000, segmentando por região do Banco Mundial (wb_regions), execute uma única consulta com subqueries que retorne o nome da região com a maior média de diferença no tempo de escolaridade entre os gêneros.
+
