f35665eaf27f552bd1cc412e8396d4ec402276a8
iamandaleao <amandabibiano144@gmail.com>
2025-09-17 19:25:19 -0300

    docs: add exercise solutions

--- mod4/uni_3/ex4/consulta.sql
+++ mod4/uni_3/ex4/consulta.sql
@@ -9,17 +9,17 @@
 | 2024-07-12 | 10,50 |
 | 2024-07-19 | 10,50 |
 +------------+-------+
-Gabarito/Comentário:
+-- Gabarito/Comentário:
 
 UPDATE relatorio_focus
 SET selic = 10.5;
        
-Realizamos o arriscado UPDATE sem a cláusula WHERE, que altera todas as linhas da tabela. No entanto, nesse caso, isso era desejado, pois nas cinco linhas existentes, o valor da taxa Selic era o mesmo. O resultado do comando informa que cinco linhas foram atualizadas. Vamos conferir:
+-- Realizamos o arriscado UPDATE sem a cláusula WHERE, que altera todas as linhas da tabela. No entanto, nesse caso, isso era desejado, pois nas cinco linhas existentes, o valor da taxa Selic era o mesmo. O resultado do comando informa que cinco linhas foram atualizadas. Vamos conferir:
 
 SELECT * 
 FROM relatorio_focus;
 
-O resultado é:
+-- O resultado é:
 2024-06-21	3.98	2.09	5.15	10.5
 2024-06-28	4	2.09	5.2	10.5
 2024-07-05	4.02	2.1	5.2	10.5
