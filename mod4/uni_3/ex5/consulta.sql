cd5b555aca45a09a6348a9079a78f0d9b3383d6a
iamandaleao <amandabibiano144@gmail.com>
2025-09-17 19:25:26 -0300

    docs: add exercise solutions

--- mod4/uni_3/ex5/consulta.sql
+++ mod4/uni_3/ex5/consulta.sql
@@ -1,13 +1,13 @@
 -- Questão 5 - Altere o campo selic para não nulo.
 
-Gabarito/Comentário:
+-- Gabarito/Comentário:
 
-O comando a seguir, apesar de estar correto, não funciona, pois o banco de dados SQLite, que estamos utilizando, não implementa essa funcionalidade até a versão atual (3.46.0).
+-- O comando a seguir, apesar de estar correto, não funciona, pois o banco de dados SQLite, que estamos utilizando, não implementa essa funcionalidade até a versão atual (3.46.0).
 
 ALTER TABLE relatorio_focus (
 MODIFY COLUMN selic NOT NULL);
 
-Portanto, precisaremos realizar essa alteração em várias etapas. O primeiro passo é criar uma tabela temporária com a estrutura desejada, incluindo a coluna selic e todos os campos configurados como não nulos.
+-- Portanto, precisaremos realizar essa alteração em várias etapas. O primeiro passo é criar uma tabela temporária com a estrutura desejada, incluindo a coluna selic e todos os campos configurados como não nulos.
 
 -- 1 - Primeiro, verificar se todos os valores de selic já estão preenchidos:
 
