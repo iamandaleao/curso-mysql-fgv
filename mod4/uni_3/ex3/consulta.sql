e0405ea861df4782d16b1bedd6ae2e1fe5c486ae
iamandaleao <amandabibiano144@gmail.com>
2025-09-17 19:25:11 -0300

    docs: add exercise solutions

--- mod4/uni_3/ex3/consulta.sql
+++ mod4/uni_3/ex3/consulta.sql
@@ -1,16 +1,16 @@
 -- Questão 3 - Adicione a taxa Selic à tabela. Primeiro, crie a coluna selic como um campo numérico com duas casas decimais e com restrição de não nulo. Se isso não for possível, crie o campo como nulo.
 
-Gabarito/Comentário:
+-- Gabarito/Comentário:
 
 ALTER TABLE relatorio_focus 
 ADD COLUMN selic NUMERIC(5,2) NOT NULL;
 
-O comando gera um erro porque a tabela já possui linhas, e o banco de dados não tem valores para preencher na nova coluna, resultando em valores NULL. Como o campo não aceita nulos, ocorre o erro. Vamos criar a coluna permitindo valores nulos:
+-- O comando gera um erro porque a tabela já possui linhas, e o banco de dados não tem valores para preencher na nova coluna, resultando em valores NULL. Como o campo não aceita nulos, ocorre o erro. Vamos criar a coluna permitindo valores nulos:
 
 ALTER TABLE relatorio_focus 
 ADD COLUMN selic NUMERIC(5,2) NULL;
 
-O resultado indica que cinco linhas foram atualizadas. Vamos verificar:
+-- O resultado indica que cinco linhas foram atualizadas. Vamos verificar:
 
 SELECT * FROM relatorio_focus;
 
